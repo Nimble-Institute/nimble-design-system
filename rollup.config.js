b7c7cf1aa0cb9f90af193af8d3ce600fa4336242
Manoj Gamachchige <Manoj>
2023-01-19 20:41:09 +0100

    added rollup

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+// To handle css files
+import postcss from "rollup-plugin-postcss";
+
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import image from "@rollup/plugin-image";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: false,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: false,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(),
+
+      terser(),
+      image(),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+
+    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports
+  },
+];
