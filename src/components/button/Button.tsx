f8f0b4debdbc4c2261aa6e64de88dd77cf0858c5
Manoj Gamachchige <Manoj>
2023-01-20 23:24:26 +0100

    added git hub actions

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,41 +1,62 @@
-import React from "react";
+import React from 'react';
+import {Button as MaterialButtom} from '@mui/material';
+import {darken, lighten} from 'polished';
 
-export interface ButtonProps
-  extends React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
-  > {
-  variant?: "primary" | "secondary" | "danger" | "success";
+interface ButtonProps {
+  label: string;
+  variant: 'primary' | 'secondary' | 'tertiary';
+  size: 'small' | 'medium' | 'large';
+  backgroundColor: string;
+  labelColor: string;
+  disabled?: boolean;
+}
+
+enum Variants {
+  primary = 'contained',
+  secondary = 'outlined',
+  tertiary = 'text',
+}
+
+enum SizeMap {
+  small = 36,
+  medium = 40,
+  large = 48,
 }
 
 export const Button: React.FC<ButtonProps> = ({
-  variant = "primary",
+  label,
+  variant = 'primary',
+  size = 'small',
+  backgroundColor = '#50606B',
+  labelColor = '#FFF',
+  disabled = false,
   ...props
 }) => {
-  const { children, style } = props;
-
-  const _style: React.CSSProperties = style || {};
-
-  switch (variant) {
-    case "primary":
-      _style.backgroundColor = "#006E7F";
-      _style.color = "#fff";
-      break;
-    case "secondary":
-      _style.backgroundColor = "#F8CB2E";
-      _style.color = "#000";
-      break;
-    case "danger":
-      _style.backgroundColor = "#B22727";
-      _style.color = "#fff";
-      break;
-  }
-
-  _style.padding = "5px 10px";
+  const selectedVariant = Variants[variant] || 'text';
+  const buttonHeight = SizeMap[size] || 36;
 
   return (
-    <button style={_style} {...props}>
-      {children}
-    </button>
+    <MaterialButtom
+      variant={selectedVariant}
+      {...props}
+      disabled={disabled}
+      sx={{
+        height: buttonHeight,
+        backgroundColor: variant === 'primary' ? backgroundColor : undefined,
+        color: labelColor,
+        border: variant !== 'tertiary' ? `2px solid ${backgroundColor}` : undefined,
+        ':hover': {
+          backgroundColor: variant === 'primary' ? darken(0.1, backgroundColor) : undefined,
+          border: variant !== 'tertiary' ? `2px solid ${darken(0.1, backgroundColor)}` : undefined,
+        },
+        ':disabled': {
+          backgroundColor: variant === 'primary' ? lighten(0.5, backgroundColor) : undefined,
+          border: variant !== 'tertiary' ? `2px solid ${lighten(0.5, backgroundColor)}` : undefined,
+          color: lighten(0.5, labelColor),
+        },
+      }}
+    >
+      {label}
+    </MaterialButtom>
   );
 };
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,23 +1,17 @@
-import { Button } from "../src";
+import {Button} from '../src';
 
 export default {
-  title: "My Component/Button",
+  title: 'Nimble Design/Button',
   component: Button,
 };
 
-const Template = (args) => ({
+const Template = args => ({
   //👇 Your template goes here
 });
 
-const PrimaryButton = Template.bind({});
-
-PrimaryButton.args = {
-  variant: "primary",
-};
-
 export const Primary = {
   args: {
-    variant: "primary",
-    children: "My First Button",
+    label: 'Press Me',
+    variant: 'primary',
   },
 };
