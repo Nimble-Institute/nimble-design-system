ee81e59bd9d2218b4689772af33eda81194cf727
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2023-07-04 12:11:13 +0200

    feat(nimble date range): added initial dev to Nimble date Range

--- src/components/nimbleDateRange/NimbleDateRange.tsx
+++ src/components/nimbleDateRange/NimbleDateRange.tsx
@@ -1,14 +1,60 @@
 import React from 'react';
-// import {DatePicker} from 'antd';
-// import {MinusOutlined} from '@ant-design/icons';
+import {DatePicker} from 'antd';
+import {MinusOutlined} from '@ant-design/icons';
 import styled from 'styled-components/dist/styled-components.js';
 
-interface NimbleDateRangeProps {}
+import {InputLabel, InputError, InputLabelProps, InputBoxProps} from '../shared';
 
-const StyledSpan = styled('span')`
-  color: red;
+interface NimbleDateRangeProps extends InputLabelProps, InputBoxProps {}
+
+const StyledRangePicker = styled(DatePicker.RangePicker)<{
+  borderColor: string;
+  activeBoxShadow: string;
+  hoverBoxShadow: string;
+}>`
+  width: 575px;
+  border-color: ${(props: {borderColor: string}) => props.borderColor};
+  box-shadow: none;
+  &:hover {
+    border: none !important;
+    box-shadow: ${(props: {hoverBoxShadow: string}) => props.hoverBoxShadow}!important;
+  }
+  &:focus {
+    border: none !important;
+    box-shadow: ${(props: {activeBoxShadow: string}) => props.activeBoxShadow}!important;
+  }
+  &:active {
+    border: none !important;
+    box-shadow: ${(props: {activeBoxShadow: string}) => props.activeBoxShadow}!important;
+  }
 `;
 
-export const NimbleDateRange: React.FC<NimbleDateRangeProps> = ({}) => {
-  return <StyledSpan>Date Range</StyledSpan>;
+export const NimbleDateRange: React.FC<NimbleDateRangeProps> = ({
+  label,
+  labelSize = 14,
+  labelWeight = '600',
+  fontFamily,
+  isRequired,
+  borderColor = '#9A9FA5',
+  activeBoxShadow = '0px 0px 0px 2px #DBF2FB, 0px 0px 0px 1px #77CBED inset',
+  hoverBoxShadow = '0px 0px 0px 2px #dae3f0, 0px 0px 0px 1px #50606B inset',
+}) => {
+  return (
+    <span>
+      <InputLabel
+        labelSize={labelSize}
+        labelWeight={labelWeight}
+        fontFamily={fontFamily}
+        isRequired={isRequired}
+        label={label}
+      />
+      <StyledRangePicker
+        size="large"
+        separator={<MinusOutlined rev={undefined} />}
+        borderColor={borderColor}
+        activeBoxShadow={activeBoxShadow}
+        hoverBoxShadow={hoverBoxShadow}
+      />
+    </span>
+  );
 };
--- stories/NimbleDateRange.stories.tsx
+++ stories/NimbleDateRange.stories.tsx
@@ -15,5 +15,12 @@ const NimbleDateRangeStory: Meta<typeof NimbleDateRange> = {
 export default NimbleDateRangeStory;
 
 export const Autocomplete = {
-  args: {},
+  args: {
+    label: 'Select Date Range',
+    labelSize: 14,
+    labelWeight: '600',
+    borderColor: '#9A9FA5',
+    activeBoxShadow: '0px 0px 0px 2px #DBF2FB, 0px 0px 0px 1px #77CBED inset',
+    hoverBoxShadow: '0px 0px 0px 2px #dae3f0, 0px 0px 0px 1px #50606B inset',
+  },
 };
