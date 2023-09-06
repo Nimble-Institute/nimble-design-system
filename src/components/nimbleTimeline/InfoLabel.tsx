e7e37998400aecd2c416e6d1b3625c3528f90b76
Anuja <anuja.ulp@gmail.com>
2023-09-06 14:48:07 +0200

    refactor label code

--- src/components/nimbleTimeline/InfoLabel.tsx
+++ src/components/nimbleTimeline/InfoLabel.tsx
@@ -1,25 +1,20 @@
-import React from "react";
-import moment from "moment";
+import React from 'react';
+import moment from 'moment';
+import {styled} from '@mui/system';
 
-export default ({ item, group, time }) => {
-  const date = moment(time, "x");
-  const label = group ? group.title : "";
-  return (
-    <div
-      style={{
-        position: "fixed",
-        left: 100,
-        bottom: 50,
-        background: "rgba(0, 0, 0, 0.5)",
-        color: "white",
-        padding: 10,
-
-        fontSize: 20,
-        borderRadius: 5,
-        zIndex: 85
-      }}
-    >
-      {`${date.format("LLL")}, ${label} `}
-    </div>
-  );
+const LabelContainer = styled('div')({
+  position: 'fixed',
+  left: 100,
+  bottom: 50,
+  background: 'rgba(0, 0, 0, 0.5)',
+  color: 'white',
+  padding: 10,
+  fontSize: 20,
+  borderRadius: 5,
+  zIndex: 85,
+});
+export default ({group, time}: {group: any; time: any}) => {
+  const date = moment(time, 'x');
+  const label = group ? group.title : '';
+  return <LabelContainer style={{}}>{`${date.format('LLL')}, ${label} `}</LabelContainer>;
 };
