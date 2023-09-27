8404105ad8829b30c8b97adccdb8ae6c8108bf11
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2023-09-27 22:22:19 +0200

    fix(nimble autocomplte): added default value fix and make controlled component using internal state

--- stories/NimbleAutocomplete.stories.tsx
+++ stories/NimbleAutocomplete.stories.tsx
@@ -36,7 +36,7 @@ export const Autocomplete = {
     placeholder: 'Input search',
     width: '500px',
     multiple: true,
-    onBlur: () => console.log('Blue'),
+    onBlur: () => console.log('Blure'),
     onChange: value => console.log(value),
   },
 };
@@ -82,5 +82,62 @@ export const AutocompleteDiabled = {
     placeholder: 'Input search',
     width: '500px',
     disabled: true,
+  },
+};
+
+export const AutocompletewithSingleSelection = {
+  args: {
+    label: 'Select Users',
+    labelSize: 14,
+    labelWeight: '600',
+    borderColor: '#9A9FA5',
+    activeBoxShadow: '0px 0px 0px 2px #DBF2FB, 0px 0px 0px 1px #77CBED inset',
+    hoverBoxShadow: '0px 0px 0px 2px #dae3f0, 0px 0px 0px 1px #9A9FA5 inset',
+    chipColor: '#9FC540',
+    data: [
+      {label: 'Option A', value: 1},
+      {label: 'Option B', value: 2},
+      {label: 'Option C', value: 3},
+      {label: 'Option D', value: 4},
+      {label: 'Option E', value: 5},
+      {label: 'Option F', value: 6},
+      {label: 'Option G', value: 7},
+      {label: 'Option H', value: 8},
+      {label: 'Option I', value: 9},
+    ],
+    placeholder: 'Input search',
+    width: '500px',
+    multiple: false,
+    onBlur: () => console.log('Blue'),
+    onChange: value => console.log(value),
+  },
+};
+
+export const AutocompleteWithSingleSelectionAndDefaultvalue = {
+  args: {
+    label: 'Select Users',
+    labelSize: 14,
+    labelWeight: '600',
+    borderColor: '#9A9FA5',
+    activeBoxShadow: '0px 0px 0px 2px #DBF2FB, 0px 0px 0px 1px #77CBED inset',
+    hoverBoxShadow: '0px 0px 0px 2px #dae3f0, 0px 0px 0px 1px #9A9FA5 inset',
+    chipColor: '#9FC540',
+    data: [
+      {label: 'Option A', value: 1},
+      {label: 'Option B', value: 2},
+      {label: 'Option C', value: 3},
+      {label: 'Option D', value: 4},
+      {label: 'Option E', value: 5},
+      {label: 'Option F', value: 6},
+      {label: 'Option G', value: 7},
+      {label: 'Option H', value: 8},
+      {label: 'Option I', value: 9},
+    ],
+    placeholder: 'Input search',
+    width: '500px',
+    multiple: false,
+    onBlur: () => console.log('Blue'),
+    onChange: value => console.log(value),
+    defaultValue: {label: 'Option A', value: 1},
   },
 };
