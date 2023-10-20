21ff4944ef3df563820184cc8812077ffded70b3
sharath <Sharath.ext@nimble.expert>
2023-10-20 16:17:55 +0530

    changed email regex value

--- src/components/nimbleInput/NimbleInput.tsx
+++ src/components/nimbleInput/NimbleInput.tsx
@@ -93,7 +93,13 @@ export const NimbleInput = forwardRef<any, NimbleInputProps>(
       onChange && onChange(value);
     };
 
-    let regex = useMemo(() => new RegExp('^[\\w.-]+@([\\w-]+\\.)+[\\w-]{2,4}$'), []);
+    let regex = useMemo(
+      () =>
+        new RegExp(
+          '(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])',
+        ),
+      [],
+    );
 
     const handleOnBlur = (event: any) => {
       if (type === 'email') {
