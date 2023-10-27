278d8a908dd2f3d43332ae55188965774207fe21
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2023-10-27 09:54:34 +0200

    perf(update storybook): storybook changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,22 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+
   framework: {
     name: '@storybook/react-webpack5',
-    options: {},
+    options: {
+      builder: {
+        fsCache: true,
+        lazyCompilation: true
+      }
+    },
   },
+
   docs: {
     autodocs: true,
   },
+
+  features: {
+    storyStoreV7: false,
+  }
 };
