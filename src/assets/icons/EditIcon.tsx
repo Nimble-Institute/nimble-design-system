77238ad36fcef8bf95db0685518a46c645bc8bb7
Anuja <anuja.ulp@gmail.com>
2023-10-29 13:50:48 +0100

    fix(svg console warnings): fix console errors caused by SVG
    fix #w57

--- src/assets/icons/EditIcon.tsx
+++ src/assets/icons/EditIcon.tsx
@@ -8,25 +8,25 @@ const DeleteIcon: React.FC<{color?: string}> = ({color = '#5989C0'}) => {
         <path
           d="M11.3335 5.33325H10.0002C6.66683 5.33325 5.3335 6.66659 5.3335 9.99992V13.9999C5.3335 17.3333 6.66683 18.6666 10.0002 18.6666H14.0002C17.3335 18.6666 18.6668 17.3333 18.6668 13.9999V12.6666"
           stroke="white"
-          stroke-width="0.9"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="0.9"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M14.6933 6.01326L9.43992 11.2666C9.23992 11.4666 9.03992 11.8599 8.99992 12.1466L8.71325 14.1533C8.60659 14.8799 9.11992 15.3866 9.84659 15.2866L11.8533 14.9999C12.1333 14.9599 12.5266 14.7599 12.7333 14.5599L17.9866 9.30659C18.8933 8.39992 19.3199 7.34659 17.9866 6.01326C16.6533 4.67992 15.5999 5.10659 14.6933 6.01326Z"
           stroke="white"
-          stroke-width="0.9"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="0.9"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M13.9399 6.7666C14.161 7.55158 14.5799 8.26664 15.1566 8.8433C15.7332 9.41995 16.4483 9.83887 17.2333 10.0599"
           stroke="white"
-          stroke-width="0.9"
-          stroke-miterlimit="10"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="0.9"
+          strokeMiterlimit="10"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
--- src/assets/icons/ExpandIcon.tsx
+++ src/assets/icons/ExpandIcon.tsx
@@ -6,15 +6,15 @@ const ExpandIcon: React.FC<{color: string}> = ({color}) => {
       <path
         d="M9.99984 18.3334C14.6023 18.3334 18.3332 14.6026 18.3332 10.0001C18.3332 5.39758 14.6023 1.66675 9.99984 1.66675C5.39734 1.66675 1.6665 5.39758 1.6665 10.0001C1.6665 14.6026 5.39734 18.3334 9.99984 18.3334Z"
         stroke={color}
-        stroke-miterlimit="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.05811 8.94995L9.99977 11.8833L12.9414 8.94995"
         stroke={color}
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
