059bfb1a4705693e663ac5f213232dba0d03db9f
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2023-12-30 21:59:30 +0100

    fix(nimble data table action icons): change background colors active and inactive

--- src/components/shared/icons/RefreshIcon.tsx
+++ src/components/shared/icons/RefreshIcon.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const RefreshIcon: React.FC<{color: string}> = ({color}) => {
+const RefreshIcon: React.FC<{color: string; backgroundcolor: string}> = ({color, backgroundcolor}) => {
   return (
     <svg width="30" height="30" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clip-path="url(#clip0_9705_13274)">
         <path
           d="M16 32C24.8368 32 32 24.8368 32 16C32 7.1632 24.8368 0 16 0C7.1632 0 0 7.1632 0 16C0 24.8368 7.1632 32 16 32Z"
-          fill="#E6EEF5"
+          fill={backgroundcolor}
         />
         <path
           d="M10 11.1191H19.8667C21.0533 11.1191 22 12.0791 22 13.2525V15.6125"
