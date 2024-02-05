982b18c4d045edd05d9a7b9f88c2ed1b44071de6
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2024-02-05 13:32:06 +0100

    feat(nimble popover select): added new component

---
+++ src/components/nimblePopoverSelect/NimblePopoverSelect.tsx
@@ -0,0 +1,156 @@
+import React, {ReactElement, useState} from 'react';
+import {Box, Typography, Popover, Button, MenuList, MenuItem} from '@mui/material';
+import {styled} from '@mui/system';
+
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import {NimbleButton} from '../nimbleButton/NimbleButton';
+import {fontWeight} from '../shared';
+
+interface ButtonProps {
+  buttoncolor: string;
+  fontFamily: string;
+  fontWeight: string;
+  labelColor?: string;
+  height?: string;
+  fontSize: string;
+}
+
+interface OptionType {
+  value: string;
+  label: string;
+}
+
+interface NimblePopoverSelectProps {
+  id: string;
+  placeholder: string;
+  selectedValue?: string;
+  startIcon?: ReactElement<any>;
+  data: OptionType[];
+  color: string;
+  fontSize?: string;
+  fontFamily?: string;
+  fontWeight?: fontWeight;
+  height?: string;
+  onChange: (value: OptionType) => void;
+}
+
+export const NimblePopoverSelect: React.FC<NimblePopoverSelectProps> = ({
+  placeholder,
+  selectedValue,
+  startIcon = undefined,
+  id,
+  data,
+  color,
+  fontSize = '14px',
+  fontFamily = 'Roboto,Helvetica,Arial,sans-serif',
+  fontWeight = '500',
+  height,
+  onChange,
+}) => {
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
+  const handleClose = (event: Event | React.SyntheticEvent) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleClickMenuItem = (event: Event | React.SyntheticEvent, selectedOption: OptionType) => {
+    onChange(selectedOption);
+    handleClose(event);
+  };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === 'Escape') {
+      setOpen(false);
+    }
+  }
+
+  const getSelectedLabel = () => {
+    const selectedOption = data.find(option => option.value === selectedValue);
+    return selectedOption?.label || placeholder;
+  };
+
+  return (
+    <>
+      <TextActionButton
+        ref={anchorRef}
+        id={id}
+        aria-controls={open ? 'composition-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleToggle}
+        variant="text"
+        endIcon={<KeyboardArrowDownIcon />}
+        startIcon={startIcon}
+        buttoncolor={color}
+        fontFamily={fontFamily}
+        fontWeight={fontWeight}
+        height={height}
+        fontSize={fontSize}>
+        {getSelectedLabel()}
+      </TextActionButton>
+      <Popover
+        open={open}
+        anchorEl={anchorRef.current}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}>
+        <MenuList
+          autoFocusItem={open}
+          id={`${id}-menu`}
+          aria-labelledby={`${id}-button`}
+          onKeyDown={handleListKeyDown}
+          sx={{
+            '&& .Mui-selected': {
+              backgroundColor: '#E2E2E2',
+            },
+          }}>
+          {data.map((option, index) => (
+            <MenuItem
+              key={option.value}
+              selected={option.value === selectedValue}
+              onClick={event => handleClickMenuItem(event, option)}
+              sx={{
+                fontFamily: fontFamily,
+                fontWeight: '400',
+                fontSize: '12px',
+              }}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Popover>
+    </>
+  );
+};
+
+const TextActionButton = styled(Button)(({buttoncolor, fontFamily, fontWeight, height, fontSize}: ButtonProps) => ({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  textTransform: 'none',
+  paddingLeft: '12px !important',
+  paddingRight: '12px !important',
+  borderRadius: '5px',
+  color: buttoncolor,
+  fontFamily,
+  fontWeight,
+  ':disabled': {
+    color: 'rgba(0, 0, 0, 0.26)',
+  },
+  height: height,
+  fontSize: fontSize,
+}));
---
+++ stories/NimblePopoverSelect.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import type {Meta} from '@storybook/react';
+
+import {NimblePopoverSelect} from '../src';
+import CheckIcon from '@mui/icons-material/Check';
+
+const NimblePopoverSelectSrtory: Meta<typeof NimblePopoverSelect> = {
+  title: 'Nimble Desktop Design/Nimble Popover Select',
+  component: NimblePopoverSelect,
+  parameters: {
+    // layout: 'fullscreen',
+    docs: {iframeHeight: 600, previewSource: 'open'},
+  },
+};
+
+export default NimblePopoverSelectSrtory;
+
+export const NimbleBasicPopoverSelect = {
+  args: {
+    placeholder: 'Approved',
+    color: '#0cc619',
+    id: '123',
+    data: [
+      {label: 'Approved', value: '1'},
+      {label: 'Billed', value: '2'},
+      {label: 'Preliminary', value: '3'},
+      {label: 'Rejected', value: '4'},
+    ],
+    selectedValue: '3',
+    onchange: value => {
+      console.log(value);
+    },
+  },
+};
+
+export const NimbleBasicPopoverSelectWithIcon = {
+  args: {
+    placeholder: 'Approved',
+    startIcon: <CheckIcon />,
+    color: '#0cc619',
+    id: '123',
+    data: [
+      {label: 'Approved', value: '1'},
+      {label: 'Billed', value: '2'},
+      {label: 'Preliminary', value: '3'},
+      {label: 'Rejected', value: '4'},
+    ],
+    onchange: value => {
+      console.log(value);
+    },
+  },
+};
